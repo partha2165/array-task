1ea621e2261eb47520be03779d72193a6a4f1868
unknown <partha2165>
2024-09-01 15:55:43 +0600

    Array Task

---
+++ arraytask.js
@@ -0,0 +1,37 @@
+// Task 1
+const fruits = [ 'apple', 'orange', 'cherry', 'grape', 'watermelon'];
+console.log(fruits[3]);
+
+// Task 2
+const touristPlace = [ 'sentmartin', 'rangamati', 'sundarban'];
+console.log(touristPlace);
+touristPlace.push('himchori');
+console.log(touristPlace);
+touristPlace.push('sajek', 'coxs bazar');
+console.log(touristPlace);
+touristPlace.pop();
+console.log(touristPlace);
+
+// Task 3 
+const books = ['macbeth', 'hamlet', 'king lear', 'javascript', 'othello'];
+console.log(books.includes('javascript'));
+if(books.includes('javascript')){
+    console.log('I will Read Macbeth.');
+}
+else('I will Read Hamlet.');
+// Shift Unshift
+const friends = [ 'Raju', 'saju', 'mithu', 'toni', 'joni'];
+console.log(friends);
+friends.shift();
+console.log(friends);
+// Task 4
+const numbers = [ 10, 15, 20, 25, 45, 70];
+console.log(Array.isArray(numbers));
+const nul = '[]'
+console.log(Array.isArray(nul));
+
+// Task 5
+
+
+
+
